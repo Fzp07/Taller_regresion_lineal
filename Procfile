eeb85751c56a5990f46283329254db27070a3ed7
Fares <fares@email.com>
2026-06-01 12:26:52 -0500

    Configure production settings for DigitalOcean App Platform

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn ia_project.wsgi --bind 0.0.0.0:8000
+web: gunicorn ia_project.wsgi --bind 0.0.0.0:$PORT
+release: python manage.py collectstatic --noinput && python manage.py migrate --noinput
--- ia_project/settings.py
+++ ia_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-121cbibq&+pf0k^3s9n#a*6)$t&yr_rziw9#xwupgssrg&xj=-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-121cbibq&+pf0k^3s9n#a*6)$t&yr_rziw9#xwupgssrg&xj=-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,3 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
